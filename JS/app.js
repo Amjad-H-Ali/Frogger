f00b8a11cad0ce4422ff3801e2877de7ee535e76
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-30 14:50:01 -0500

    Fixed Legs And Arms;Added Event Listeners

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,32 @@
 //Get Canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-ctx.beginPath();
+//Add an evnt listener to DOC
+document.addEventListener('keydown',(e)=>{
+	const key = e.key;
+	console.log(key);
+	//Change Frogger Direction When User clicks on Arrow Keys
+	if(key === 'ArrowRight'){
+		frogger.direction = 'E';
+	}
+	else if(key === 'ArrowLeft'){
+		frogger.direction = 'W';
+	}
+	else if(key === 'ArrowUp' ){
+		frogger.direction = 'N';
+	}
+	else if(key === 'ArrowDown'){
+		frogger.direction = 'S';
+	}
+})
+
+
 //Make Frog Object
 const frogger = {
 	//How Many Lives our Hero will Have
 	life: 5,
-	
+	//Direction Frog Moves
+	direction: 'N',
 	//Position hero in center of width of canvas
 	x: canvas.width/2,
 	y: canvas.height - 50,
@@ -15,33 +35,38 @@ const frogger = {
 	color: 'rgb(102,153,0)',
 	//Function to draw body
 	drawFrog(){
-	
-		ctx.moveTo(this.x - 15, this.y - 15)
-		ctx.lineTo(this.x  , this.y)
-		// ctx.strokeStyle = this.color;
-		
-		ctx.moveTo(this.x + 15, this.y + 15)
-		ctx.lineTo(this.x , this.y)
-		// ctx.strokeStyle = this.color;
+		ctx.lineWidth = 3;
 
-		ctx.moveTo(this.x + 15, this.y - 15)
-		ctx.lineTo(this.x , this.y)
-		// ctx.strokeStyle = this.color;
-
-		ctx.moveTo(this.x - 15, this.y + 15)
-		ctx.bezierCurveTo(this.x , this.y,this.x ,this.x ,this.x ,this.x )
+		ctx.beginPath();
+		ctx.moveTo(this.x - 15, this.y - 15);
+		ctx.lineTo(this.x + 15 , this.y + 15);
 		ctx.strokeStyle = this.color;
 
 		ctx.stroke();
-		 
- 		console.log(this);
+
 		ctx.beginPath();
+		ctx.moveTo(this.x - 15, this.y + 15);
+		ctx.lineTo(this.x + 15 , this.y - 15);
+		ctx.strokeStyle = this.color;
+		ctx.stroke();
+
+		ctx.beginPath();
+ 		console.log(this);
 		ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
 		ctx.fillStyle = this.color;
-		
-		
 		ctx.fill();
-	}
+	},
+	moveFrog(direction){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+	},
+
+	changeDirection(){
+
+	},
+
+
 
 	
 }

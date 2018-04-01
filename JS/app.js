7a91dccbd0cb1e8947a76a22402ff7f8192d90b5
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-01 14:51:40 -0500

    Created Logs

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,8 @@
 //Global Vars
 //The Value to increment/Decrement X,Y when Moving Frog
 let speed = 50;
+
+//Generates Random Color For Cars
 const randomColor = ()=>{
 	let red, green, blue;
 
@@ -14,7 +16,7 @@ const randomColor = ()=>{
 
 
 }
-
+//Get Random Size For Cars 50-149
 const getRandomSize = () =>{
 	return Math.floor((Math.random() * 100) + 50)
 }
@@ -104,105 +106,147 @@ const scene = {
 	},
 	//The Safe Zone
 	land:{
-			x: 0,//Draw From
-			y: 0,//Draw From
-			w: canvas.width,//Size of land Horizontally
-			h: canvas.height/7,//Size of Land Vertically
-			color:'#B2F699',//Color Of Land
-				drawLand(){ //Draw 3 Peices of land
-						for(let i = 0; i < 3; i ++){
-						ctx.beginPath();
-						ctx.rect(this.x , i * 300, this.w, this.h)
-						ctx.fillStyle = this.color;
-						ctx.fill();
-						ctx.closePath();
+		x: 0,//Draw From
+		y: 0,//Draw From
+		w: canvas.width,//Size of land Horizontally
+		h: canvas.height/7,//Size of Land Vertically
+		color:'#B2F699',//Color Of Land
+		drawLand(){ //Draw 3 Peices of land
+			for(let i = 0; i < 3; i ++){
+				ctx.beginPath();
+				ctx.rect(this.x , i * 300, this.w, this.h)
+				ctx.fillStyle = this.color;
+				ctx.fill();
+				ctx.closePath();
 						
-						}
+			}
 
-			},
+		},
 
 	},
 	//Danger zone is the water and Street
 	dangerZone:{
-				x: 0,
-				y: canvas.height/7,//Where to draw from
-				w: canvas.width,
-				h: canvas.height/3.5, //Two Times bigger than land
-				color: ['#1491CB','rgb(0,0,0)'],
-				//Water Object
-				water:{
-					drawWater(){ 
-					
-						ctx.beginPath();
-						ctx.rect(scene.dangerZone.x , scene.dangerZone.y , scene.dangerZone.w, scene.dangerZone.h)
-						ctx.fillStyle = scene.dangerZone.color[0];
-						ctx.fill();
-						ctx.closePath();
-						// Reset the value of y for next time
-						scene.dangerZone.y = canvas.height/7;
-					}
-					
-					
-				},
-				//Street Object
-				street:{
-					rows: [ 
-						{
-							name:'row1',
-							x:0,
-							y:530,
-							speed:8,
-							vehicles:[]
-						},
-					 	{	name:'row2',
-							x:250,
-							y:470,
-							speed:-8,
-							vehicles:[]
-
-					 	},
-					 	{
-					 		name:'row3',
-							x:450,
-							y:410,
-							speed:10,
-							vehicles:[]
-					 	}
-					 ],
-					drawStreet(){
-						ctx.beginPath();
-						ctx.rect(scene.dangerZone.x , scene.dangerZone.y + 300 , scene.dangerZone.w, scene.dangerZone.h)
-						ctx.fillStyle = scene.dangerZone.color[1];
-						ctx.fill();
-						ctx.closePath();
-						// reset the value of y for next time
-						scene.dangerZone.y = canvas.height/7;
+			x: 0,
+			y: canvas.height/7,//Where to draw from
+			w: canvas.width,
+			h: canvas.height/3.5, //Two Times bigger than land
+			color: ['#1491CB','rgb(0,0,0)','#C28F2E'],
+			//Water Object
+			water:{
+				//Rows In The Water
+				rows: 
+				[ 
+					{
+						name:'row1',
+						x:0,
+						y:237,
+						speed:2,
+						vehicles:[]
 					},
-					vehicleFactory(){
-						for(let i = 0; i < this.rows.length; i ++){
-							for(let j = 0; j < 3; j ++){
-								const newVehicle = new Vehicle(this.rows[i].x + (200 * j), this.rows[i].y, getRandomSize(), 30, randomColor() , this.rows[i].speed, this.rows[i].name);
-								this.rows[i].vehicles.push(newVehicle);
-								this.rows[i].vehicles[j].drawVehicle();
-							}
-						}	
+					{	
+						name:'row2',
+						x:250,
+						y:175,
+						speed:-3,
+						vehicles:[]
+
+					 },
+					 {
+					 	name:'row3',
+						x:450,
+						y:112.5,
+						speed:4,
+						vehicles:[]
+					 }
+				],
+				//Draw Water
+				drawWater(){ 
+					
+					ctx.beginPath();
+					ctx.rect(scene.dangerZone.x , scene.dangerZone.y , scene.dangerZone.w, scene.dangerZone.h)
+					ctx.fillStyle = scene.dangerZone.color[0];
+					ctx.fill();
+					ctx.closePath();
+					// Reset the value of y for next time
+					scene.dangerZone.y = canvas.height/7;
+				},
+				//Generate Logs And Push To Each Row
+				logFactory(){	
+					for(let i = 0; i < this.rows.length; i ++){
+						for(let j = 0; j < 3; j ++){
+							const newLog = new Log(this.rows[i].x + (200 * j), this.rows[i].y, 150, 50, scene.dangerZone.color[2], this.rows[i].speed, this.rows[i].name);
+							this.rows[i].vehicles.push(newLog);
+							this.rows[i].vehicles[j].drawVehicle();
+						}
+					}	
+					
+				}
+			},
+			//Street Object
+			street:{
+				//Rows In the Street
+				rows: 
+				[ 
+					{
+						name:'row1',
+						x:0,
+						y:530,
+						speed:5,
+						vehicles:[]
+					},
+					{	
+						name:'row2',
+						x:250,
+						y:470,
+						speed:-5,
+						vehicles:[]
+
+					},
+					{
+					 	name:'row3',
+						x:450,
+						y:410,
+						speed:8,
+						vehicles:[]
+					 }
+				],
+				//Draw Street
+				drawStreet(){
+					ctx.beginPath();
+					ctx.rect(scene.dangerZone.x , scene.dangerZone.y + 300 , scene.dangerZone.w, scene.dangerZone.h)
+					ctx.fillStyle = scene.dangerZone.color[1];
+					ctx.fill();
+					ctx.closePath();
+					// reset the value of y for next time
+					scene.dangerZone.y = canvas.height/7;
+				},
+				//Generate Vehicles And Push To Each Row
+				vehicleFactory(){
+					for(let i = 0; i < this.rows.length; i ++){
+						for(let j = 0; j < 3; j ++){
+							const newVehicle = new Vehicle(this.rows[i].x + (200 * j), this.rows[i].y, getRandomSize(), 30, randomColor() , this.rows[i].speed, this.rows[i].name);
+							this.rows[i].vehicles.push(newVehicle);
+							this.rows[i].vehicles[j].drawVehicle();
+						}
+					}	
 						
 
-					}
 				}
+			}
 			
 	}	
 }
 
+//Make A Vehicle Class
 class Vehicle {
-	constructor(x,y,w,h,color,speed,row){
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.color = color;
-		this.speed = speed;
-		this.row = row;
+		constructor(x,y,w,h,color,speed,row){
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+			this.color = color;
+			this.speed = speed;
+			this.row = row;
 		}
 		drawVehicle(){
 			ctx.beginPath();
@@ -229,10 +273,16 @@ class Vehicle {
 			this.drawVehicle();
 
 				
-				
-				
 		}
 	
+}
+
+//Make A log Class
+class Log extends Vehicle{
+	constructor(x,y,w,h,color,speed,row){
+		super(x, y, w, h, color, speed, row);
+	}
+
 
 }
 
@@ -241,16 +291,28 @@ class Vehicle {
 
 
 
-const animate = ()=>{
 
+const animate = ()=>{
+	//Draw Scene
 	scene.drawScene();
+
+	// Draw Each Log
+	for(let i = 0, r = scene.dangerZone.water.rows.length; i < r ; i ++){
+		for(let j = 0, v = scene.dangerZone.water.rows[i].vehicles.length ; j < v ; j ++){
+			scene.dangerZone.water.rows[i].vehicles[j].move();
+		}
+	}
+	//Draw Frogger
 	frogger.drawFrog();	
 
+	//Draw Each Car
 	for(let i = 0, r = scene.dangerZone.street.rows.length; i < r ; i ++){
 		for(let j = 0, v = scene.dangerZone.street.rows[i].vehicles.length ; j < v ; j ++){
 			scene.dangerZone.street.rows[i].vehicles[j].move();
 		}
 	}
+
+
 	
 
 	window.requestAnimationFrame(animate);
@@ -260,6 +322,7 @@ scene.drawScene();
 frogger.drawFrog();
 
 scene.dangerZone.street.vehicleFactory()
+scene.dangerZone.water.logFactory()
 
 
 
